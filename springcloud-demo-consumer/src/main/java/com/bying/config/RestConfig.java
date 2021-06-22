504c191c266bf884d1fb3b92f610377431c7e753
chengkunxf <chengkunxf@126.com>
2021-06-22 15:30:20 +0800

    fix: 打印请求日志

--- springcloud-demo-consumer/src/main/java/com/bying/config/RestConfig.java
+++ springcloud-demo-consumer/src/main/java/com/bying/config/RestConfig.java
@@ -3,7 +3,11 @@ package com.bying.config;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author chengkunxf@126.com
@@ -15,7 +19,15 @@ public class RestConfig {
 
     @LoadBalanced//使用Ribbon实现负载均衡的调用 (spring cloud -> 封装ribbon + eureka + restTemplate)
     @Bean
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        List<ClientHttpRequestInterceptor> list = new ArrayList<>();
+        list.add((request, body, execution) -> {
+            System.out.println("当前请求的URL是：" + request.getURI().toString());
+            return execution.execute(request, body);
+        });
+        restTemplate.setInterceptors(list);
+
+        return restTemplate;
     }
 }
